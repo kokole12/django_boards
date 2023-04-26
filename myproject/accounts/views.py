7d633ec7bfaf261065c7942d8699158b80dddd2a
kokol12 <kokoleismail886@gmail.com>
2023-04-26 14:13:40 +0300

    added additional filed in the signup page

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login as auth_login
+from .forms import SignUpForm
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm()
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
+            auth_login(request, user)
             return redirect('home')
         else:
             form = UserCreationForm()
